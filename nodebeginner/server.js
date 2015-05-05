b7f0c8574c68267842ac87ae5a21fcaa57dbc485
spekulatif <ufuktandogan@windowslive.com>
2015-05-06 00:49:03 +0300

    Add router.js

--- nodebeginner/server.js
+++ nodebeginner/server.js
@@ -1,13 +1,19 @@
 var http = require("http");
+var url = require("url");
 
-function start(){
+function start(route) {
   function onRequest(request, response) {
-    console.log("Request received.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }
-  http.createServer(onRequest).listen(8888);
-}
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    route(pathname);
 
-exports.start = start;
+     response.writeHead(200, {"Content-Type": "text/plain"});
+     response.write("Hello World");
+     response.end();
+   }
+
+   http.createServer(onRequest).listen(8888);
+   console.log("Server has started.");
+ }
+
+ exports.start = start;
