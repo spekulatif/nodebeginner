3566760c6b95b99711baeb199c382833da754e4f
spekulatif <ufuktandogan@windowslive.com>
2015-05-06 18:43:12 +0300

    reorganised requestHandler.js

--- nodebeginner/requestHandlers.js
+++ nodebeginner/requestHandlers.js
@@ -22,12 +22,28 @@ function start(response, postData) {
   response.end();
 }
 
-function upload(response, postData) {
-  console.log("Request handler 'upload' was called.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent: " + querystring.parse(postData).text);
-  response.end();
-}
+ function upload(response, request) {
+   console.log("Request handler 'upload' was called.");
+
+   var form = new formidable.IncomingForm();
+   console.log("about to parse");
+   form.parse(request, function(error, fields, files) {
+     console.log("parsing done");
+
+     /* Possible error on Windows systems:
+     tried to rename to an already existing file */
+     fs.rename(files.upload.path, "/tmp/test.png", function(error) {
+       if (error) {
+         fs.unlink("/tmp/test.png");
+         fs.rename(files.upload.path, "/tmp/test.png");
+     }
+     });
+     response.writeHead(200, {"Content-Type": "text/html"});
+     response.write("received image:<br/>");
+     response.write("<img src='/show' />");
+     response.end();
+   });
+ }
 
 function show(response) {
   console.log("Request handler 'show' was called.");
@@ -37,3 +53,4 @@ function show(response) {
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
