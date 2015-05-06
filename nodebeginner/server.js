d6ccb45565c8290c8dd53561c31565fe8bbfae86
spekulatif <ufuktandogan@windowslive.com>
2015-05-06 15:42:22 +0300

    Add post data function

--- nodebeginner/server.js
+++ nodebeginner/server.js
@@ -3,9 +3,20 @@ var url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response) {
+    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
     
+    request.setEncoding("utf8");
+
+    request.addListener("data", function(postDataChunk){
+      postData += postDataChunk;
+      console.log("received POST data chunk '"+postDataChunk + "'.");
+    });
+    
+    request.addListener("end", function(){
+      route(handle, pathname, response, postData);
+    });
     route(handle, pathname, response);
   }
 
